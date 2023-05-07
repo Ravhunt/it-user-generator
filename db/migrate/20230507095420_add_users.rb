f0338439dcb6177ff45250a6e6e06312955f1616
Ravhunt <Raviel54@gmail.com>
2023-05-07 20:13:33 +0200

    requested edits

--- db/migrate/20230507095420_add_users.rb
+++ db/migrate/20230507095420_add_users.rb
@@ -3,9 +3,7 @@ class AddUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :username
-      t.string :email
-      t.string :password
+      null :false
     end 
   end
 end
