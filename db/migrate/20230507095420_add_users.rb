1033d90417d44eaf2c6d4d99d5119001a64aeec9
Ravhunt <Raviel54@gmail.com>
2023-05-07 20:15:25 +0200

    changed to t.null

--- db/migrate/20230507095420_add_users.rb
+++ db/migrate/20230507095420_add_users.rb
@@ -3,7 +3,7 @@ class AddUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      null :false
+      t.null :false
     end 
   end
 end
