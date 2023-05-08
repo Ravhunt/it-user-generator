afe51b1031680b2abf0b302421ad670bbda97e29
Ravhunt <Raviel54@gmail.com>
2023-05-08 20:41:39 +0200

    added null, changed indentations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,25 @@
 class UsersController < ApplicationController
-	def index
-		@user = User.new
+  def index
+    @user = User.new
 	end
 
 	def create
-		@user = User.new(user_params)
+    @user = User.new(user_params)
 
-		if @user.save
-			redirect_to @user
-		else
-			render :index, status: :unprocessable_entity
-		end
-	end
+    if @user.save
+      redirect_to @user
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 
-	def show
-		@user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
 	end
 
 	private
 
 	def user_params
-		params.require(:user).permit(:first_name, :last_name)
-
-	end
+    params.require(:user).permit(:first_name, :last_name)
+  end
 end
--- db/migrate/20230507095420_add_users.rb
+++ db/migrate/20230507095420_add_users.rb
@@ -1,9 +1,8 @@
 class AddUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.null :false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
     end 
   end
 end
