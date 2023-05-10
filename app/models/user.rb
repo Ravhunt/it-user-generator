416a167561b85732368387bf8bf2b83e5cd34b17
Ravhunt <Raviel54@gmail.com>
2023-05-10 21:09:00 +0200

    added random generated password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 
+  def password(length = 8)
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    password = ''
+    length.times { password << chars.sample }
+    password
+  end
+
 end
