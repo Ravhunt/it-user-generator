11de0a78909ced631a5f08cca75c206c59c76732
Ravhunt <Raviel54@gmail.com>
2023-05-15 17:34:23 +0200

    Removed # messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,21 @@
 class User < ApplicationRecord
-  # ...
 
   def password
     alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
     special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?']
     digits = (0..9).to_a
 
-    # Generate a password with one random character from each of the three categories
     password = []
     password << alphabet.sample
     password << special_chars.sample
     password << digits.sample
 
-    # Generate the remaining characters randomly from all three categories
     (1..5).each do |i|
       char_category = [alphabet, special_chars, digits].sample
       password << char_category.sample
     end
 
-    password.shuffle.join[0..7] # Take the first 8 characters of the shuffled password
+    password.shuffle.join[0..7]
   end
 
-  # ...
 end
