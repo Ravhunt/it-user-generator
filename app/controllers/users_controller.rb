094797c4fbc3ebf3e258e3f4ba3b56dc66f598bf
Ravhunt <Raviel54@gmail.com>
2023-05-16 18:31:29 +0200

    STUFF

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @user = User.new
   end
@@ -7,19 +8,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render :index, status: :unprocessable_entity
     end
   end
 
   def show
-    @user = User.find(params[:id])
+    if session[:user_id].present?
+      @user = User.find(session[:user_id])
+      clear_user_data
+    else
+      redirect_to root_path
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name)
+  end
+
+  def clear_user_data
+    @user = User.find(params[:id])
+    @user.delete
   end
 end
