b26f2a7146d0637b14ca3adf223b35a34f1c5378
Ravhunt <Raviel54@gmail.com>
2023-05-16 14:30:13 +0200

    Added username generation, email generation, changed the password to do 3 lower case , 3 number and 3 Upper case letters, made the rspec changed to include all of these as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,43 @@
 class User < ApplicationRecord
+  before_validation :capitalize_names
 
   def password
-    alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
-    special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?']
+    lowercase_letters = ('a'..'z').to_a
+    uppercase_letters = ('A'..'Z').to_a
     digits = (0..9).to_a
 
-    password = []
-    password << alphabet.sample
-    password << special_chars.sample
-    password << digits.sample
+    password = ''
+    
+    password += lowercase_letters.sample(3).join
 
-    (1..5).each do |i|
-      char_category = [alphabet, special_chars, digits].sample
-      password << char_category.sample
-    end
+    password += digits.sample(3).join
 
-    password.shuffle.join[0..7]
+    password += uppercase_letters.sample(3).join
+
+    password
+  end
+
+  def username
+    first_initial = first_name[0].downcase
+    last_name_normalized = last_name.downcase.gsub(/\s+/, '') # Remove any whitespace from last_name
+
+    "#{first_initial}#{last_name_normalized}"
+  end
+
+  def email
+    first_initial = first_name[0].capitalize
+    last_name_normalized = last_name.downcase.gsub(/\s+/, '') # Remove any whitespace from last_name
+    domain = "@tbrown.com"
+
+    "#{first_name.capitalize}.#{last_name.capitalize}#{domain}"
+  end
+
+
+private
+
+  def capitalize_names
+    self.first_name = first_name.capitalize if first_name.present?
+    self.last_name = last_name.capitalize if last_name.present?
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,16 +13,32 @@ RSpec.describe UsersController, type: :controller do
         post :create, params: { user: user_params }
       end.to change(User, :count).by(1)
 
+      user = User.last
+      expect(user.email).to eq("John.Smith@tbrown.com")
+      expect(user.first_name).to eq("John")
+      expect(user.last_name).to eq("Smith")
+
       expect(response).to redirect_to(User.last)
     end
 
-    it "generates a random password for new user" do
-      user = User.new
+    it "generates a random password with 3 lowercase letters, 3 random numbers, and 3 uppercase letters" do
+      post :create, params: { user: { first_name: "John", last_name: "Smith" } }
+      user = User.last
       password = user.password
-      expect(password.length).to eq(8)
-      expect(password).to match(/[a-zA-Z]/)
-      expect(password).to match(/\d/)
-      expect(password).to match(/[^a-zA-Z\d]/)
+
+      expect(password.length).to eq(9) # 3 lowercase letters + 3 digits + 3 uppercase letters
+      expect(password).to match(/^[a-z]{3}[0-9]{3}[A-Z]{3}$/)
     end
+
+    it "generates a username from the first letter of first_name and the full last_name" do
+      post :create, params: { user: { first_name: "John", last_name: "Smith" } }
+      user = User.last
+      expected_username = "jsmith"
+
+      expect(user.username).to eq(expected_username)
+    end
+
+
+    
   end
 end
